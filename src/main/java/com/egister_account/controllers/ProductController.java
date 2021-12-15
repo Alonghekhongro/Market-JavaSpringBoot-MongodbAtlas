44646157842df9b592202b6609b7686df3a9ac15
Alonghekhongro <roughrider3003@gmail.com>
2021-12-15 16:11:44 +0700

    java

--- src/main/java/com/egister_account/controllers/ProductController.java
+++ src/main/java/com/egister_account/controllers/ProductController.java
@@ -1,9 +1,6 @@
 package com.egister_account.controllers;
 
-import com.egister_account.models.Product;
 import com.egister_account.models.StoreProduct;
-import com.egister_account.repositories.ProductRepository;
-import com.egister_account.services.ProductServiceImp;
 import com.egister_account.services.StoreProductServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @CrossOrigin
 @RestController
@@ -22,9 +18,13 @@ public class ProductController {
     @Autowired
     StoreProductServiceImp storeProductServiceImp;
     @GetMapping("/find")
-    public ResponseEntity<List<StoreProduct>> FindProduct(@RequestParam(value = "name", defaultValue = "")
+    public ResponseEntity<?> FindProduct(@RequestParam(value = "name", defaultValue = "")
                                                      String name) {
         try {
+            List<StoreProduct> storeProduct = storeProductServiceImp.getStoreProduct(name);
+            if(storeProduct.isEmpty()){
+                return new ResponseEntity<>("Not found",HttpStatus.NOT_FOUND);
+            }
             return new ResponseEntity<>(storeProductServiceImp.getStoreProduct(name),
                     HttpStatus.OK);
         } catch (Exception e) {
