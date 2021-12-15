d1306c1dedbf9352f6d7bfa7c1cd3bee489f0f57
Alonghekhongro <roughrider3003@gmail.com>
2021-12-15 15:20:35 +0700

    java

--- src/main/java/com/egister_account/repositories/StoreProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.egister_account.repositories;
-
-import com.egister_account.models.StoreProduct;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface StoreProductRepository extends MongoRepository<StoreProduct, String> {
-}
--- src/main/java/com/egister_account/services/StoreProductServiceImp.java
+++ src/main/java/com/egister_account/services/StoreProductServiceImp.java
@@ -4,14 +4,12 @@ import com.egister_account.models.Product;
 import com.egister_account.models.Store;
 import com.egister_account.models.StoreProduct;
 import com.egister_account.repositories.ProductRepository;
-import com.egister_account.repositories.StoreProductRepository;
 import com.egister_account.repositories.StoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class StoreProductServiceImp implements StoreProductService{
@@ -19,8 +17,6 @@ public class StoreProductServiceImp implements StoreProductService{
     ProductRepository productRepository;
     @Autowired
     StoreRepository storeRepository;
-    @Autowired
-    StoreProductRepository storeProductRepository;
     @Override
     public List<StoreProduct> getStoreProduct(String product_name){
         List<Product> list_product =  productRepository.search(product_name);
@@ -33,7 +29,8 @@ public class StoreProductServiceImp implements StoreProductService{
                     temptPList.add(product);
                 }   
             }
-            StoreProduct storeProduct = new StoreProduct(store.getStore_name(),store.getAddress(), temptPList);
+            StoreProduct storeProduct = new StoreProduct(
+                    store.getStore_name(), store.getAddress(), temptPList);
             list_store_product.add(storeProduct);
         }
         return list_store_product;
