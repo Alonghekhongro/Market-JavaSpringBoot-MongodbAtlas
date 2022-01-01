4d3ed0d990a049a59175e5924ceef459c8a21376
Alonghekhongro <roughrider3003@gmail.com>
2022-01-01 20:49:18 +0700

    java

--- src/main/java/com/market/controllers/admin_StoreController.java
+++ src/main/java/com/market/controllers/admin_StoreController.java
@@ -1,11 +1,15 @@
 package com.market.controllers;
 
+import com.market.models.Register;
 import com.market.models.Store;
+import com.market.services.RegisterServiceImp;
 import com.market.services.StoreServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -13,6 +17,8 @@ import org.springframework.web.bind.annotation.*;
 public class admin_StoreController {
     @Autowired
     StoreServiceImp storeServiceImp;
+    @Autowired
+    RegisterServiceImp registerServiceImp;
     @PostMapping("/reviewstore")
     public ResponseEntity<?> ReviewRegisterSale(@RequestBody Store store){
         try{
@@ -22,6 +28,18 @@ public class admin_StoreController {
                         "have been insert" , HttpStatus.NOT_FOUND);
             }
             return new ResponseEntity<>(st,HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @GetMapping("/storeregister")
+    public ResponseEntity<?> GetAllStoreRegister(){
+        try{
+            List<Register> re = registerServiceImp.getAllRegister();
+            if (re.isEmpty()){
+                return new ResponseEntity<>("No store register found", HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(re,HttpStatus.OK);
         } catch (Exception e){
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
